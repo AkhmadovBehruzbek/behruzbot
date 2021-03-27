eefa8bc33dd1ccab96949722652d9d921513ccce
AkhmadovBehruzbek <78653457+AkhmadovBehruzbek@users.noreply.github.com>
2021-03-27 16:25:40 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?
 date_default_timezone_set('Asia/Tashkent');
 define('API_KEY', '1640307159:AAGVcT5S69YSZNPi2C476y0ISSRm93eCPvk');
-$Manager = "1322664602";
+$Manager = "941327405";
 $compane = "infomir.uz LTD";
 function bot($method, $datas = []){
     $url = "https://api.telegram.org/bot".API_KEY."/" . $method;
