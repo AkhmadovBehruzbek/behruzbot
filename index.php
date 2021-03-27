3beffb7689e5a2fdbc98de20338505592a10cbf9
AkhmadovBehruzbek <78653457+AkhmadovBehruzbek@users.noreply.github.com>
2021-03-27 12:49:09 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
 <?php 
-    define('API_KEY', '1712032695:AAH81uQUOCsAf3umM_IzjGtUYF5jBa_N8mw');
-
-    function bot ($method, $datas=[]) {
-        $url = "https://api.telegram.org/bot".API_KEY."/".$method;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
-        $res = curl_exec($ch);
-        if (curl_error($ch)) {
-            var_dump(curl_error($ch));
-        } else {
-            return json_decode($res);
-        }
-    }
-
-    // typing... function
-    function typing ($ch) {
-        return bot('sendChatAction', [
-            'chat_id' => $ch,
-            'action' => 'typing',
-        ]);
-    }
-
-    $update = json_decode(file_get_contents('php://input'));
-    $message = $update->message;
-    $chat_id = $message->chat->id;
-    $tx = $message->text;
-
-    if (isset($text)) {
-        typing($chat_id);
-    }
-
-    if ($text == "/start") {
-        bot('sendMessage', [
-            'chat_id' => $chat_id,
-            'text' => "Assalomu alaykum",
-        ]);
-    }
+    $data = file_get_contents('php://input');
+    $data = json_decode($data, true);
+    file_put_contents(__DIR__.'/message.txt', print_r($data, true));
 
 ?>
