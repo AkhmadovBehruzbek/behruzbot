8478bd36607dad170014903b31ee59a8899ff3f9
AkhmadovBehruzbek <78653457+AkhmadovBehruzbek@users.noreply.github.com>
2021-03-27 17:19:43 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,31 @@
-<?
-date_default_timezone_set('Asia/Tashkent');
-define('API_KEY', '1640307159:AAGVcT5S69YSZNPi2C476y0ISSRm93eCPvk');
-$Manager = "1322664602";
-$compane = "infomir.uz LTD";
-function bot($method, $datas = []){
-    $url = "https://api.telegram.org/bot".API_KEY."/" . $method;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
-    $res = curl_exec($ch);
-    curl_close($ch);
-    if (!curl_error($ch)) return json_decode($res);
-};
-function html($text){
-    return str_replace(['<','>'],['&#60;','&#62;'],$text);
-};
+<?php 
+	$botToken = "1640307159:AAGVcT5S69YSZNPi2C476y0ISSRm93eCPvk";
+	$website = "https://api.telegram.org/bot".$botToken;
 
-$update = json_decode(file_get_contents('php://input'));
-// testlog
-//file_put_contents("log.txt",file_get_contents('php://input'));
-// message variables
-$message = $update->message;
-$text = html($message->text);
-$chat_id = $message->chat->id;
-// $from_id = $message->from->id;
-// $message_id = $message->message_id;
-$first_name = $message->from->first_name;
-$last_name = $message->from->last_name;
-$full_name = html($first_name . " " . $last_name);
+	function curl_get_contents($url) {
+		$ch = curl_init();
 
-// // replymessage
-// $reply_to_message = $message->reply_to_message;
-// $reply_chat_id = $message->reply_to_message->forward_from->id;
-// $reply_text = $message->text;
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
 
+		$data = curl_exec($ch);
+		curl_close($ch);
 
-// Agar yozgan odam $Manager bo'lmasa ushbu kod qismiga kiramiz
-// if ($chat_id != $Manager) {
-    // Agar yozilgan habar /start bolsa, yani yangi foydalanuvchi
-    //  botni ishga tushursa ushbu kod bajariladi
-    if ($text == "/start") {
-        $reply = "Assalom Alaykum <b>" . $full_name . "</b>, " . $compane . " Qabul Botiga Xush Kelibsiz !\nMurojat Yo'llashingiz Mumkin 👇";
-        bot('sendmessage', [ // maxsus bot funksiyamiz orqali sendmessage ga
-            'chat_id' => $chat_id, //foydalanuvchi id raqami va
-            'text' => $reply, // habar matnini
-            'parse_mode' => "HTML", //html formatda yuboramiz.
-        ]);
-}
+		return $data;
+	}
+
+	$update = curl_get_contents($website."php://input");
+
+    $updateArray = json_decode($update, TRUE);
+
+    $chat_id = $updateArray["result"][0]["message"]["chat"]["id"];
+
+    $first_name = $updateArray["result"]["0"]["message"]["from"]["first_name"];
+
+    curl_get_contents($website."/sendMessage?chat_id=".$chat_id."&text=Assalomu alaykum, ".$first_name);
+    
+    echo $first_name;
+    
+    print_r($chat_id);
+    ?>
