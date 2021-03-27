02027ac1936802e78cb2615c3d3dcc20fa4306d4
AkhmadovBehruzbek <78653457+AkhmadovBehruzbek@users.noreply.github.com>
2021-03-27 15:27:45 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php 
-    $data = file_get_contents('php://input');
-    $data = json_decode($data, true);
-    file_put_contents(__DIR__.'/message.txt', print_r($data, true));
+    define('API_KEY', '1640307159:AAGVcT5S69YSZNPi2C476y0ISSRm93eCPvk');
+    $Manager = "1322664602";
+    
+    function bot($method, $datas=[]) {
+        $url = "https://api.telegram.org".API_KEY."/".$method;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+        
+        $res = curl_exec($ch);
+
+        if (!curl_error($ch)) return json_decode($res);
+    };
+
+    function html ($text) {
+        return str_replace(['<','>'],['&#60','&#62;'], $text);
+    };
+    
+    $update = json_decode(file_get_contents('php://input'));
+
+    // testlog
+    file_put_contents("log.txt", file_get_contents('php://input'));
+
+    
+
 
 ?>
