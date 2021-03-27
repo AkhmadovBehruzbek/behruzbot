941c05c4cf93190bb3d1997b01baae542c4a6f61
AkhmadovBehruzbek <78653457+AkhmadovBehruzbek@users.noreply.github.com>
2021-03-27 16:44:45 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,90 @@
-<?
-date_default_timezone_set('Asia/Tashkent');
-define('API_KEY', '1640307159:AAGVcT5S69YSZNPi2C476y0ISSRm93eCPvk');
-$Manager = "941327405";
-$compane = "infomir.uz LTD";
-function bot($method, $datas = []){
-    $url = "https://api.telegram.org/bot".API_KEY."/" . $method;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
-    $res = curl_exec($ch);
-    curl_close($ch);
-    if (!curl_error($ch)) return json_decode($res);
-};
-function html($text){
-    return str_replace(['<','>'],['&#60;','&#62;'],$text);
-};
+<?php 
+    date_default_timezone_set('Asia/Tashkent');
+    define('API_KEY', '1640307159:AAGVcT5S69YSZNPi2C476y0ISSRm93eCPvk');
+    $Manager = "1322664602";
+    
+    function bot($method, $datas=[]) {
+        $url = "https://api.telegram.org".API_KEY."/".$method;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+        
+        $res = curl_exec($ch);
 
-$update = json_decode(file_get_contents('php://input'));
-// testlog
-file_put_contents("log.txt",file_get_contents('php://input'));
-// message variables
-$message = $update->message;
-$text = html($message->text);
-$chat_id = $message->chat->id;
-$from_id = $message->from->id;
-$message_id = $message->message_id;
-$first_name = $message->from->first_name;
-$last_name = $message->from->last_name;
-$full_name = html($first_name . " " . $last_name);
-
-// replymessage
-$reply_to_message = $message->reply_to_message;
-$reply_chat_id = $message->reply_to_message->forward_from->id;
-$reply_text = $message->text;
-
-
-// Agar yozgan odam $Manager bo'lmasa ushbu kod qismiga kiramiz
-if ($chat_id != $Manager) {
-    // Agar yozilgan habar /start bolsa, yani yangi foydalanuvchi
-    //  botni ishga tushursa ushbu kod bajariladi
-    if ($text == "/start") {
-        // Foydalanuvchiga Manager  yoki kompaniya nomidan salom yo'llaymiz.
-        $reply = "Assalom Alaykum <b>" . $full_name . "</b>, " . $compane . " Qabul Botiga Xush Kelibsiz !\nMurojat Yo'llashingiz Mumkin 👇";
-        bot('sendmessage', [ // maxsus bot funksiyamiz orqali sendmessage ga
-            'chat_id' => $chat_id, //foydalanuvchi id raqami va
-            'text' => $reply, // habar matnini
-            'parse_mode' => "HTML", //html formatda yuboramiz.
-        ]);
-        //  Yangi foydalanuvchi malumotlarini manajerga aniq vaqt bilan yuboramiz.
-        $reply = "Yangi mijoz:\n" . $full_name . "\n👉 👉 <a href='tg://user?id=" . $from_id . "'>" . $from_id . "</a>\n" . date('Y-m-d H:i:s') . "";
-        bot('sendmessage', [ // maxsus bot funksiyamiz orqali sendmessage ga
-            'chat_id' => $Manager, //Manager id raqami va
-            'text' => $reply, // habar matnini
-            'parse_mode' => "HTML", //html formatda yuboramiz.
-        ]);
-        // Foydalanuvchidan kelgan ilk /start habarini javob bera olishi uchun managerga yuboramiz.
-        bot('forwardMessage', [ // maxsus bot funksiyamiz orqali forwardMessage ga
-            'chat_id' => $Manager, //Manager id raqami va
-            'from_chat_id' => $chat_id, // foydalanuvchi bilan bot o'rtasidagi chat id raqami
-            'message_id' => $message_id, // va foydalanuvchi yuborgan habar id raqamini yuboramiz.
-        ]);
-        // Tekshiramiz foydalanuvchi /start komandasidan boshqa narsa yozgan bo'lsa
-    }else if ($text != "/start"){
-        // Foydalanuvchidan kelgan habarni javob bera olishi uchun managerga yuboramiz.
-        bot('forwardMessage', [ // maxsus bot funksiyamiz orqali forwardMessage ga
-            'chat_id' => $Manager, //Manager id raqami va
-            'from_chat_id' => $chat_id, // foydalanuvchi bilan bot o'rtasidagi chat id raqami
-            'message_id' => $message_id, // va foydalanuvchi yuborgan habar id raqamini yuboramiz.
-        ]);
+        if (!curl_error($ch)) return json_decode($res);
     }
-    // Yoki agar $Manager yozgan bo'lsa ushbu kod qismiga kiramiz
-}else if($chat_id == $Manager){
-    // Agar manager bot qayta yuborgan hatga javob berish orqali habar yuborsa,
-    if(isset($reply_to_message)){
-        // Manager habarini bot qayta yuborgan habar egasiga bot nomidan yuboramiz
-        bot('sendmessage', [ // maxsus bot funksiyamiz orqali sendmessage ga
-            'chat_id' => $reply_chat_id, // bot qayta yuborgan habar id raqami va
-            'text' => $reply_text, // manager yuborgan habarni
-            'parse_mode' => "HTML", //html formatda yuboramiz.
-        ]);
+
+   
+
+    function html($text){
+        return str_replace(['<','>'],['&#60;','&#62;'],$text);
     }
-    // Manager profilidan botni tekshirib ko'rish uchun botdan managerga salom !
-    if($text == "hi" or $text == "/start"){
-        bot('sendmessage', [
-            'chat_id' => $Manager,
-            'text' => "Salom Manager !",
-        ]);
+    
+    $update = json_decode(file_get_contents('php://input'));
+
+    // testlog
+    file_put_contents("log.txt", file_get_contents('php://input'));
+
+    // message variables
+    $message = $update->message;
+    $text = html($message->text);
+    $chat_id = $message->chat->id;
+    $from_id = $message->from->id;
+    $message_id = $message->message_id;
+    $first_name = $message->from->first_name;
+    $last_name = $message->from->last_name;
+    $full_name = html($first_name." ".$last_name);
+
+    // reply message
+    $reply_to_message = $message->reply_to_message;
+    $reply_chat_id = $message->reply_to_message->forward_from->id;
+    $reply_text = $message->text;
+
+    if ($chat_id != $Manager) {
+        if ($text == "/start") {
+            $reply = "Assalomu alaykum <b>".$full_name."</b>, Botga xush kelibsiz!  \n Murojaat yo'llash mumkin👇";
+            
+            bot ('sendMessage', [
+                'chat_id' => $chat_id,
+                'text' => $reply,
+                'parse_mode' => "HTML",
+            ]);
+
+            $reply = "Yangi mijoz:\n".$full_name."\n👉👉<a hreff='tg://user?id=".$from_id."'>".$from_id. "</a>\n".date('Y-m-d H:i:s'). "";
+            bot ('sendMessage', [
+                'chat_id' => $Manager,
+                'text' => $reply,
+                'parse_mode' => "HTML",
+            ]);
+
+            bot ('forwardMessage', [
+                'chat_id' => $Manager,
+                'from_chat_id' => $chat_id,
+                'message_id' => $message_id,
+            ]);
+        } else if ($text != "/start") {
+            bot ('forwardMessage', [
+                'chat_id' => $Manager,
+                'from_chat_id' => $chat_id,
+                'massage_id' => $message_id,
+            ]);
+        }
+    } else if ($chat_id == $Manager){
+        if (isset($reply_to_message)) {
+            bot('sendMessage', [
+                'chat_id' => $reply_chat_id,
+                'text' => $reply_text,
+                'parse_mode' => "HTML",
+            ]);
+        }
+        if ($text == "hi" or $text == "/start") {
+            bot('sendMessage', [
+                'chat_id' => $Manager,
+                'text' => "salom Menejer!",
+            ]);
+        }
     }
-}
+
+
+?>
